4c010e6c90722b4f6242c0e172b398706088c3cb
HemanthK-12 <hemanthkarthikeya12@gmail.com>
2023-10-17 16:26:14 +0530

    Working Pose Estimation with comments and clear explanation

--- arucoPoseEstimation.py
+++ arucoPoseEstimation.py
@@ -3,9 +3,13 @@ import cv2
 import sys
 import time
 
+#Here we define all possible 21 aruco marker types present in the openCV library
 
+#DICT_4*4_50 
+#Here,4*4=size of the aruco marker with 4*4 grid of black and white squares.
+#50 means the dictionary in which the markers are stored,typically meaning it has 50 possible combinations of markers.
 ARUCO_DICT = {
-	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+	"DICT_4X4_50": cv2.aruco.DICT_4X4_50, 
 	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
 	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
 	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
@@ -53,69 +57,39 @@ def aruco_display(corners, ids, rejected, image):
 			cY = int((topLeft[1] + bottomRight[1]) / 2.0)
 			cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
 			
-			cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-				0.5, (0, 255, 0), 2)
+			cv2.putText(image, str(markerID),(topLeft[0], topLeft[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 			print("[Inference] ArUco marker ID: {}".format(markerID))
 			
 	return image
 
+#Distortion coefficients and intrinsic paramters are diff for every camera and need to be calibrated using an aruco marker of known length and id.This is done in the folowing two lines.
+distortioncoeffs = np.array([0, 0, 0, 0], dtype=np.float32)
+intrinsic_parameters = np.array([[800, 0, 320], [0, 800, 240], [0, 0, 1]], dtype=np.float32)
 
+#Selecting the type of aruco markers we'll be showcasing
+aruco_type = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_1000)
 
-def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
+#Edge length of aruco markers,only for calibration,here for redundancy
+sidelength_marker = 0.02
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    cv2.aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)
-    parameters = cv2.aruco.DetectorParameters_create()
-
-
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(gray, cv2.aruco_dict,parameters=parameters,
-        cameraMatrix=matrix_coefficients,
-        distCoeff=distortion_coefficients)
-
-        
-    if len(corners) > 0:
-        for i in range(0, len(ids)):
-           
-            rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients,
-                                                                       distortion_coefficients)
-            
-            cv2.aruco.drawDetectedMarkers(frame, corners) 
-
-            cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)  
-
-    return frame
-
-
-    
-
-aruco_type = "DICT_5X5_100"
-
-arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
-
-arucoParams = cv2.aruco.DetectorParameters_create()
-
-
-intrinsic_camera = np.array(((933.15867, 0, 657.59),(0,933.1586, 400.36993),(0,0,1)))
-distortion = np.array((-0.43948,0.18514,0,0))
-
-
+#This turns on the webcam.
 cap = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    parameters = cv2.aruco.DetectorParameters_create()
+    corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(gray, aruco_type, parameters=parameters)
 
+    if corners:
+        rotationvecs, translationvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, sidelength_marker, intrinsic_parameters, distortioncoeffs)
+        print(f"{rotationvecs} \n")
+        for i in range(len(ids)):
+            frame = cv2.drawFrameAxes(frame, intrinsic_parameters, distortioncoeffs,rotationvecs[i],translationvecs[i], 0.02)  
+    cv2.aruco.drawDetectedMarkers(frame, corners)
+    cv2.imshow('frame', cv2.flip(frame, 1))
 
-
-while cap.isOpened():
-    
-    ret, img = cap.read()
-    
-    output = pose_estimation(img, ARUCO_DICT[aruco_type], intrinsic_camera, distortion)
-
-    cv2.imshow('Estimated Pose', output)
-
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('k'):
         break
 
 cap.release()
